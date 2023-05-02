29e2e58dff90668cbc916e632ff9db71e5158304
JonasOlsson90 <olssonjonas@hotmail.com>
2023-05-02 13:37:21 +0200

    Move IO logic to separate class

---
+++ infinitum/IO/IO.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography;
+using infinitum.core;
+
+namespace infinitum.IO;
+
+public class IO
+{
+    private readonly SHA256 _sha256;
+
+    public IO()
+    {
+        _sha256 = SHA256.Create();
+    }
+
+    public string GetPrivateKey()
+    {
+        // Check if local private key exists
+        if (!File.Exists("./privatekey.txt"))
+        {
+            // If not generate a new one
+            return Guid.NewGuid().ToString();
+        }
+
+        // ToDo: create from file
+        return "";
+    }
+
+    public string GetPublicKey(string privateKey)
+    {
+        // Generate public key by hashing private key
+        return _sha256.ComputeHash(System.Text.Encoding.ASCII.GetBytes(privateKey)).ToString();
+    }
+
+    public List<Block> GetLocalBlockchain(string publicKey)
+    {
+        if (!File.Exists("./blockchain.txt"))
+        {
+            // If not create blockchain with genisis block
+
+            // Create genisis block
+            var transactions = new List<Transaction>();
+
+            var transaction = new Transaction()
+            {
+                Amount = 1000,
+                Sender = null,
+                Recipient = publicKey,
+                TimeStamp = DateTime.Now.Ticks
+            };
+            transactions.Add(transaction);
+
+            var genisisBlock = new Block(0, _sha256.ComputeHash(System.Text.Encoding.ASCII.GetBytes(String.Empty)), transactions);
+
+            var blockchain = new List<Block>();
+
+            blockchain.Add(genisisBlock);
+
+            return blockchain;
+        }
+
+        //ToDo: Get from file
+        return new List<Block>();
+    }
+}
--- infinitum/Program.cs
+++ infinitum/Program.cs
@@ -1,5 +1,4 @@
-using System.Security.Cryptography;
-using infinitum.core;
+using infinitum.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,60 +13,14 @@ var app = builder.Build();
 // ToDo: Remove this and add real endpoints
 app.MapGet("/ping", () => "I has been pinged");
 
+
 var client = new HttpClient();
+var io = new IO();
 
 // Check if local blockchain exists
 
-List<Block> blockchain;
-string privateKey;
-string publicKey;
-
-var sha = SHA256.Create();
-
-
-// Check if local private key exists
-if (!File.Exists("./privatekey.txt"))
-{
-    // If not generate a new one
-    privateKey = Guid.NewGuid().ToString();
-}
-else
-{
-    // ToDo: create from file
-    privateKey = "";
-}
-
-
-// Generate public key by hashing private key
-publicKey = sha.ComputeHash(System.Text.Encoding.ASCII.GetBytes(privateKey)).ToString();
-
-if (!File.Exists("./blockchain.txt"))
-{
-    // If not create blockchain with genisis block
-
-    // Create genisis block
-    var transactions = new List<Transaction>();
-
-    var transaction = new Transaction()
-    {
-        Amount = 1000,
-        Sender = null,
-        Recipient = publicKey,
-        TimeStamp = DateTime.Now.Ticks
-    };
-
-    transactions.Add(transaction);
-
-    var genisisBlock = new Block(0, sha.ComputeHash(System.Text.Encoding.ASCII.GetBytes(String.Empty)), transactions);
-
-    blockchain = new();
-
-    blockchain.Add(genisisBlock);
-}
-else
-{
-    // ToDo: create from file
-}
-
+var privateKey = io.GetPrivateKey();
+var publicKey = io.GetPublicKey(privateKey);
+var blockchain = io.GetLocalBlockchain(publicKey);
 
 app.Run();
